9c00a1bfa363f700aeb3511d957757696a2131c3
Никита <nickit.crasnov@gmail.com>
2022-04-01 18:45:22 +0300

    first

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ruble_exchange_rate.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
