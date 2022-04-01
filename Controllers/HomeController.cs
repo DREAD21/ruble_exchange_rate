93da2e07453fcbe1a41403684ca0465d160f786b
Никита <nickit.crasnov@gmail.com>
2022-04-01 20:33:11 +0300

    first

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,10 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
 using ruble_exchange_rate.Models;
 
 namespace ruble_exchange_rate.Controllers
@@ -17,9 +20,24 @@ namespace ruble_exchange_rate.Controllers
         {
             _logger = logger;
         }
-
+        [Route("/")]
         public IActionResult Index()
         {
+            DateTime date = DateTime.Today;
+            string url = string.Format("http://api.currencylayer.com/live?access_key=2bca30d02f7b0504ae1b2e160dd15c1a");
+            HttpWebRequest httpWebRequest = (HttpWebRequest)WebRequest.Create(url);
+            HttpWebResponse httpWebResponse = (HttpWebResponse)httpWebRequest.GetResponse();
+            string response;
+            using (StreamReader streamReader = new StreamReader(httpWebResponse.GetResponseStream()))
+            {
+                response = streamReader.ReadToEnd();
+            }
+            RubleInfo Response = JsonConvert.DeserializeObject<RubleInfo>(response);
+            var bucks = Response.quotes.USDRUB;
+            var euro = Response.quotes.USDEUR;
+            ViewBag.eu = string.Format("{0:0.00}", bucks/euro);
+            ViewBag.buck = string.Format("{0:0.00}", bucks);
+            ViewBag.date_ = date.ToString("D");
             return View();
         }
 
