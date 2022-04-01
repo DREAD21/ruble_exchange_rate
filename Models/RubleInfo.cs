85c143d25f2500b8038d48aca5d3f0a6e454dfde
Никита <nickit.crasnov@gmail.com>
2022-04-01 20:33:31 +0300

    2

---
+++ Models/RubleInfo.cs
@@ -0,0 +1,14 @@
+﻿namespace ruble_exchange_rate.Models
+{
+    
+    public class RubleInfo
+    {
+        public Info quotes { get; set; }
+    }
+
+    public class Info
+    {
+        public decimal USDRUB { get; set; }
+        public decimal USDEUR { get; set; }
+    }
+}
